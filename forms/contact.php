107700c1f418ccd8e28ba6c3f58ba7665657d3b2
Nitya Guntaka <66152719+nitti02@users.noreply.github.com>
2024-02-12 12:38:06 -0600

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -26,9 +26,9 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
   $contact->smtp = array(
-    'host' => 'nityaguntaka.com',
+    'host' => 'gmail.com',
     'username' => 'nitti02',
-    'password' => 'pass',
+    'password' => 'Bangaram02',
     'port' => '587'
   );
   
